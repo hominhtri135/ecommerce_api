668586f76c5f9087896b1308b33b1f3aaf5c8eea
Minh Tri <hominhtri135@gmail.com>
2023-11-22 14:40:50 +0700

    ADD Inventory Model

---
+++ src/models/repositories/inventory.repo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const { Types } = require("mongoose");
+const { inventory } = require("../inventory.model");
+
+const insertInventory = async ({ productId, shopId, stock, location }) => {
+  return await inventory.create({
+    inven_productId: productId,
+    inven_shopId: shopId,
+    inven_stock: stock,
+    inven_location: location,
+  });
+};
+
+module.exports = { insertInventory };
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -7,6 +7,7 @@ const {
   electronic,
   furniture,
 } = require("~/models/product.model");
+const { insertInventory } = require("~/models/repositories/inventory.repo");
 const {
   findAllDraftsForShop,
   publishProductByShop,
@@ -114,7 +115,17 @@ class Product {
 
   //create new product
   async createProduct(product_id) {
-    return await product.create({ ...this, _id: product_id });
+    const newProduct = await product.create({ ...this, _id: product_id });
+    if (newProduct) {
+      // add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct._id,
+        shopId: this.product_shop,
+        stock: this.product_quantity,
+      });
+    }
+
+    return newProduct;
   }
 
   //update product
